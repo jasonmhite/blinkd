c5dac5258841ae8db80386ddb2ee624408bc4964
Jason Hite <jasonmhite@gmail.com>
2015-03-04 12:03:42 -0500

    Working

--- client.py
+++ client.py
@@ -1,8 +1,15 @@
 import asyncio
 
+SOCKET_ADDRESS = '127.0.0.1'
+SOCKET_PORT = 59993
+
 @asyncio.coroutine
 def echo_client(msg, loop):
-    reader, writer = yield from asyncio.open_unix_connection('/var/run/blinkd', loop=loop)
+    reader, writer = yield from asyncio.open_connection(
+        SOCKET_ADDRESS,
+        SOCKET_PORT,
+        loop=loop,
+    )
 
     print('Send: {}'.format(msg))
     writer.write(msg.encode())
@@ -12,7 +19,11 @@ def echo_client(msg, loop):
 
     writer.close()
 
-message = "Hello"
+#@asyncio.coroutine
+#def blink_client(msg, loop):
+    #reader, writer = yield from asyncio.open_unix_connection('/var/run/blinkd', loop=loop)
+
+message = "pulse 10 10 10 2 1000 60"
 loop = asyncio.get_event_loop()
 loop.run_until_complete(echo_client(message, loop))
 
--- daemon.py
+++ daemon.py
@@ -1,21 +1,46 @@
 import asyncio
-import protocol
+import os
+
+# Simple client:
+# echo "cmd ..args.." | nc 127.0.0.1 59993
+
+from protocol import COMMANDS
+
+SOCKET_ADDRESS = '127.0.0.1'
+SOCKET_PORT = 59993
+
+loop = asyncio.get_event_loop()
+#coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
+lock = asyncio.Lock(loop=loop)
 
 @asyncio.coroutine
-def handle_echo(reader, writer):
+def handle_command(reader, writer):
     data = yield from reader.read(100)
-    msg = data.decode()
+    args = data.decode().strip().split(" ")
+    cmd = args.pop(0)
 
-    print("Received: {}".format(msg))
+    print("Command: {}".format(cmd))
+    print("Args: {}".format(args))
 
-    writer.write("OK".encode())
+    with (yield from lock):
+        r = COMMANDS[cmd].run(*args)
+
+    if r is None:
+        writer.write("OK".encode())
+    else:
+        writer.write(str(r).encode())
+
     yield from writer.drain()
 
     writer.close()
 
-loop = asyncio.get_event_loop()
-#coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
-coro = asyncio.start_unix_server(handle_echo, '/var/run/blinkd', loop=loop)
+#coro = asyncio.start_unix_server(handle_command, SOCKET_ADDRESS, loop=loop)
+coro = asyncio.start_server(
+    handle_command,
+    SOCKET_ADDRESS,
+    SOCKET_PORT,
+    loop=loop,
+)
 server = loop.run_until_complete(coro)
 
 try:
